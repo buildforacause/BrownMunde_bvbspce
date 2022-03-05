7daaaad44d521dd342a4cc6dc55deb27972e46aa
Harmit Saini <92787535+buildforacause@users.noreply.github.com>
2022-03-05 18:27:55 +0530

    Updating the webapp

--- app.py
+++ app.py
@@ -1,11 +1,87 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager
+from forms import LoginForm, RegisterForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///campus.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin, db.Model):
+    tablename = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():
     return render_template("index.html")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already used')
+            return redirect(url_for('login'))
+        hashed_salted_psw = generate_password_hash(
+            password,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+            name=form.name.data,
+            email=email,
+            password=hashed_salted_psw,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('get_all_posts'))
+    return render_template("register.html", form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash('email or password incorrect')
+            return redirect(url_for('login', form=form))
+        elif user and not check_password_hash(user.password, password):
+            flash('email or password incorrect')
+            return redirect(url_for('login', form=form))
+        login_user(user)
+        return redirect(url_for('get_all_posts'))
+    return render_template("login.html", form=form)
 
 
 if __name__ == '__main__':
